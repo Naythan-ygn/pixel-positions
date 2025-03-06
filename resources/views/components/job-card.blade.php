669f3bbab555d237885def31eb9e65718e89d3a4
HlaingPhyoeThant <naythanprime@gmail.com>
2025-03-07 00:54:17 +0630

    Initial Commit

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,19 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">
+        Laracasts
+    </div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img src="https://i.pinimg.com/736x/fe/eb/69/feeb69cbfdf6fc658f3280e85bdc1032.jpg" alt="" width="42"
+            class="rounded-xl">
+    </div>
+</div>
